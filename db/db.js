66df65b2464d7d83dd88a5a216834c7465689b6f
vineet <vineet_mishra@intuit.com>
2018-04-17 22:50:53 +0530

    mongodb link added

--- db/db.js
+++ db/db.js
@@ -3,4 +3,5 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongodb_url = "mongodb://vineet30:Buntymis30!@ds141889.mlab.com:41889/modulegenerator";
 
+mongoose.connect(mongodb_url);
 module.exports = {mongoose};
