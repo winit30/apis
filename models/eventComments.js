564162ebd8a1ac73a770384c7a2dd3a50855d447
vineet <vineet_mishra@intuit.com>
2018-05-08 05:34:39 +0530

    reply function added

--- models/eventComments.js
+++ models/eventComments.js
@@ -59,7 +59,7 @@ CommentSchema.statics.findCommentById = function(_id) {
 CommentSchema.methods.saveCommentReply = function(body) {
     var comment = this;
     comment.replies.push(body);
-    return comment.save().then((comment) => {
+    return comment.save().then(() => {
         return comment;
     });
 }
