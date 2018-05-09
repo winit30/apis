0c0da1a038c410d9ac6f5375ac52f83808b8b438
vineet <vineet_mishra@intuit.com>
2018-05-10 02:16:31 +0530

    comments file updated

--- routes/commentsRoutes.js
+++ routes/commentsRoutes.js
@@ -60,11 +60,9 @@ Router.delete('/deleteComment/:id', authenticate, (req, res) => {
     const body = req.body;
     console.log(body);
     Event.findEventById(body.eventId).then((event) => {
-        console.log(event);
         const deleteQuery = {
             _id: req.params.id
         }
-        console.log(event.organizerId, req.user._id);
         if(event.organizerId+"" !== req.user._id+"") {
             deleteQuery.commentedby = req.user._id;
         }
