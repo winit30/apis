e209ced9a859a529febf73ec1c980579e82509ca
vineet <vineet_mishra@intuit.com>
2018-06-07 00:50:14 +0530

    x-auth added

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const applicationRoutes = require('./../routes/applicationRoutes');
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-auth");
   next();
 });
 //Set Request Method
